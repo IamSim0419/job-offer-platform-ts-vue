7a87c47666d088b0ee3f0c419674a621e2a3a91a
Simreich Somogod <simreich1989@gmail.com>
2025-09-09 09:28:13 +0800

    path issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  // server: {
+  //   fs: {
+  //     strict: false,
+  //   },
+  // },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)), // Base alias
